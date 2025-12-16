914692568ea23c44a0d221650566926070ba7ac4
SquareSpheres <yngvenot+github@pm.me>
2025-12-16 20:52:22 +0100

    Refactor to use static library for core logic
    Moved core implementation to a new static library 'pingpong_lib' for better modularity and testability. Updated CMake configuration to link the main executable and tests against this library. Added a test for GetSomeChar and removed trivial arithmetic tests.

--- project1-ping-pong/tests/test_main.cpp
+++ project1-ping-pong/tests/test_main.cpp
@@ -1,16 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "common/utils.hpp"
+#include "SomeFile.h"
 
 TEST_CASE("greet returns formatted greeting") {
     REQUIRE(common::greet("World") == "Hello, World!");
     REQUIRE(common::greet("Test") == "Hello, Test!");
 }
 
-TEST_CASE("1+1 == 2") {
-    REQUIRE(1 + 1 == 2);
-}
-
-TEST_CASE("1+1+3 == 5") {
-    REQUIRE(1 + 1 + 3 == 5);
+TEST_CASE("GetSomeChar returns a dot") {
+    REQUIRE(GetSomeChar() == '.');
 }
